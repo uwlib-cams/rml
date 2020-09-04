cfcb49c1efc8d8792fe14e3e34a9f6d252525fa4
mcm104 <mcm104@uw.edu>
2020-09-03 21:56:43 -0700

    Update script

--- findBadFiles.py
+++ findBadFiles.py
@@ -1,18 +1,21 @@
 import os
+from sys import argv
+
+script, fileDate = argv
 
 bad_files = {}
 bad_input_file_count = 0
 
 """For input files"""
 
-workList = os.listdir('input/2020_9_2/work')
-expressionList = os.listdir('input/2020_9_2/expression')
-manifestationList = os.listdir('input/2020_9_2/manifestation')
-itemList = os.listdir('input/2020_9_2/item')
+workList = os.listdir(f'input/{fileDate}/work')
+expressionList = os.listdir(f'input/{fileDate}/expression')
+manifestationList = os.listdir(f'input/{fileDate}/manifestation')
+itemList = os.listdir(f'input/{fileDate}/item')
 
 for work in workList:
     workID = work.split(".")[0]
-    with open(f'input/2020_9_2/work/{work}', 'r') as file:
+    with open(f'input/{fileDate}/work/{work}', 'r') as file:
         identifier_list = []
         for line in file:
             if '<rdf:Description rdf:about="https://trellis.sinopia.io/repository/washington/' in line:
@@ -34,7 +37,7 @@ for work in workList:
 
 for expression in expressionList:
     expressionID = expression.split(".")[0]
-    with open(f'input/2020_9_2/expression/{expression}', 'r') as file:
+    with open(f'input/{fileDate}/expression/{expression}', 'r') as file:
         identifier_list = []
         for line in file:
             if '<rdf:Description rdf:about="https://trellis.sinopia.io/repository/washington/' in line:
@@ -56,7 +59,7 @@ for expression in expressionList:
 
 for manifestation in manifestationList:
     manifestationID = manifestation.split(".")[0]
-    with open(f'input/2020_9_2/manifestation/{manifestation}', 'r') as file:
+    with open(f'input/{fileDate}/manifestation/{manifestation}', 'r') as file:
         identifier_list = []
         for line in file:
             if '<rdf:Description rdf:about="https://trellis.sinopia.io/repository/washington/' in line:
@@ -78,7 +81,7 @@ for manifestation in manifestationList:
 
 for item in itemList:
     itemID = item.split(".")[0]
-    with open(f'input/2020_9_2/item/{item}', 'r') as file:
+    with open(f'input/{fileDate}/item/{item}', 'r') as file:
         for line in file:
             if '<rdf:Description rdf:about="https://trellis.sinopia.io/repository/washington/' in line:
                 identifier = line.strip()
@@ -99,16 +102,16 @@ for item in itemList:
 
 """For output files"""
 
-work1List = os.listdir('output/2020_9_2/work_1')
-work2List = os.listdir('output/2020_9_2/work_2')
-instanceList = os.listdir('output/2020_9_2/instance')
-itemList = os.listdir('output/2020_9_2/item')
+work1List = os.listdir(f'output/{fileDate}/work_1')
+work2List = os.listdir(f'output/{fileDate}/work_2')
+instanceList = os.listdir(f'output/{fileDate}/instance')
+itemList = os.listdir(f'output/{fileDate}/item')
 
 bad_output_file_count = 0
 
 for work_1 in work1List:
     work1ID = work_1.split(".")[0]
-    with open(f'output/2020_9_2/work_1/{work_1}', 'r') as file:
+    with open(f'output/{fileDate}/work_1/{work_1}', 'r') as file:
         identifier_list = []
         for line in file:
             if "> a bf:Work " in line:
@@ -129,7 +132,7 @@ for work_1 in work1List:
 
 for work_2 in work2List:
     work2ID = work_2.split(".")[0]
-    with open(f'output/2020_9_2/work_2/{work_2}', 'r') as file:
+    with open(f'output/{fileDate}/work_2/{work_2}', 'r') as file:
         identifier_list = []
         for line in file:
             if "> a bf:Work " in line:
@@ -150,7 +153,8 @@ for work_2 in work2List:
 
 for instance in instanceList:
     instanceID = instance.split(".")[0]
-    with open(f'output/2020_9_2/instance/{instance}', 'r') as file:
+    with open(f'output/{fileDate}/instance/{instance}', 'r') as file:
+        identifier_list = []
         for line in file:
             if "> a bf:Instance " in line:
                 identifier = line.split('>')[0]
@@ -170,7 +174,8 @@ for instance in instanceList:
 
 for item in itemList:
     itemID = item.split(".")[0]
-    with open(f'output/2020_9_2/item/{item}', 'r') as file:
+    with open(f'output/{fileDate}/item/{item}', 'r') as file:
+        identifier_list = []
         for line in file:
             if "> a bf:Item " in line:
                 identifier = line.split('>')[0]
@@ -197,7 +202,7 @@ with open('badFileReport.txt', 'w') as output_file:
         input_file_list_length = len(input_file_list)
         output_file_list_length = len(output_file_list)
 
-        output_file.write(f"Identifier: {identifier}\nNumber of times repeated: {input_file_list_length}\nAppears in:\n")
+        output_file.write(f"Identifier: {identifier}\nNumber of times repeated: {input_file_list_length + output_file_list_length}\nAppears in:\n")
         if input_file_list_length > 0:
             output_file.write("Input files:\n")
             for file_ in input_file_list:
