ed5ccbacaf7aa2fc896beaedfcbbf8513bff7a4a
mcm104 <mcm104@uw.edu>
2021-03-10 15:22:17 -0800

    Rewrite type_dates.py for XML

--- old/scripts/type_dates.py
+++ old/scripts/type_dates.py
@@ -1,246 +1,94 @@
 import os
 from datetime import date
-#from sys import argv
 
-#script, data_directory = argv
+"""Variables"""
 
 today = date.today()
-currentDate = str(today).replace('-', '_')
+currentDate = str(today).replace('-','_')
 
-def fix_work_1_output(work1List):
-	for work in work1List:
-		with open(f"output/{currentDate}/work_1/{work}", "r") as input_file: # open it up
-			row_count = 0 # start at 0
-			output_file_lines = [] # start with empty list
-			for line in input_file: # iterate throgh each line in the input file
-				if "@prefix" in line: # if the line contains "@prefix"
-					row_count += 1 # add one to row count
-					output_file_lines.append(line)
+"""Functions"""
 
-				elif "rdfs:" in line:
-					if "rml.py SNAPSHOT:" in line:
-						spacing = line.split('rdfs:')[0]
-						edited_line = line.strip()
-						punctuation = line.split('\"')[-1]
-						edited_line = edited_line.strip(punctuation)
-						edited_line = edited_line.strip()
-						edited_line = spacing + edited_line + f"^^xsd:dateTime {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else:
-						output_file_lines.append(line)
+def type_snapshot(line):
+	spacing = line.split('rdfs:')[0]
+	edited_line = line.strip()
 
-				else:
-					# isolate the name of the property
-					property_ = line.split('bf:')[-1]
-					property_ = property_.split(' ')[0]
+	punctuation = edited_line.split('"')[-1]
 
-					if property_ in bf_date_prop_list:
-						spacing = line.split('bf:')[0] # preserve whitespace at the beginning of the line
-						edited_line = line.strip() # remove whitespaces
-						dash_count = 0 # start at 0
-						for character in edited_line: # iterate through characters in line
-							if character == "-":
-								dash_count += 1 # count the number of dashes
-						punctuation = edited_line.split('\"')[-1] # see if line ends in ;, ,, or .
-						if dash_count == 0: # no dashes, e.g. "1886"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYear {strip(punctuation)}\n"
-						elif dash_count == 1: # one dash, e.g. "1886-02"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYearMonth {strip(punctuation)}\n"
-						elif dash_count == 2: # two dashes, e.g. "1886-02-14"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:date {strip(punctuation)}\n"
-						output_file_lines.append(edited_line) # add to list
-					else: # if the line does not have a date in it
-						output_file_lines.append(line) # add it to list with no edits
+	edited_line = edited_line.strip(punctuation)
+	edited_line = spacing + edited_line + "^^xsd:dateTime" + punctuation + "\n"
 
-			output_file_lines[row_count] = output_file_lines[row_count].strip() + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n" # add prefix for datatypes after last prefix
-		with open(f"output/{currentDate}/work_1/{work}", "w") as output_file:
-			for line in output_file_lines:
-				output_file.write(line)
+	return edited_line
 
-def fix_work_2_output(work2List):
-	for work in work2List:
-		with open(f"output/{currentDate}/work_2/{work}", "r") as input_file: # open it up
-			row_count = 0 # start at 0
-			output_file_lines = [] # start with empty list
-			for line in input_file: # iterate throgh each line in the input file
-				if "@prefix" in line: # if the line contains "@prefix"
-					row_count += 1 # add one to row count
-					output_file_lines.append(line)
+def determine_date_type(line):
+	value = line.split('"')[1]
 
-				elif "rdfs:" in line:
-					if "rml.py SNAPSHOT:" in line:
-						spacing = line.split('rdfs:')[0]
-						edited_line = line.strip()
-						punctuation = "] " + edited_line[-1] # see if line ends in ;, ,, or .
-						edited_line = edited_line.strip(punctuation)
-						edited_line = edited_line.strip()
-						edited_line = spacing + edited_line + f"^^xsd:dateTime {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else:
-						output_file_lines.append(line)
+	dash_count = 0
+	for character in value:
+		if character == '-':
+			dash_count += 1
 
-				else:
-					# isolate the name of the property
-					property_ = line.split('bf:')[-1]
-					property_ = property_.split(' ')[0]
+	if dash_count == 0: # e.g. "1886"
+		date_type = "gYear"
+	elif dash_count == 1: # e.g. "1886-02"
+		date_type = "gYearMonth"
+	elif dash_count == 2: # e.g. "1886-02-14"
+		date_type = "date"
 
-					if property_ in bf_date_prop_list:
-						spacing = line.split('bf:')[0] # preserve whitespace at the beginning of the line
-						edited_line = line.strip() # remove whitespaces
-						dash_count = 0 # start at 0
-						for character in edited_line: # iterate through characters in line
-							if character == "-":
-								dash_count += 1 # count the number of dashes
-						punctuation = edited_line[-1] # see if line ends in ;, ,, or .
-						if dash_count == 0: # no dashes, e.g. "1886"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYear {punctuation}\n"
-						elif dash_count == 1: # one dash, e.g. "1886-02"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYearMonth {punctuation}\n"
-						elif dash_count == 2: # two dashes, e.g. "1886-02-14"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:date {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else: # if the line does not have a date in it
-						output_file_lines.append(line) # add it to list with no edits
+	return date_type
 
-			output_file_lines[row_count] = output_file_lines[row_count].strip() + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n" # add prefix for datatypes after last prefix
-		with open(f"output/{currentDate}/work_2/{work}", "w") as output_file:
-			for line in output_file_lines:
-				output_file.write(line)
+def type_date(line, date_type):
+	spacing = line.split('bf:')[0]
+	edited_line = line.strip()
 
-def fix_instance_output(instanceList):
-	for instance in instanceList:
-		with open(f"output/{currentDate}/instance/{instance}", "r") as input_file: # open it up
-			row_count = 0 # start at 0
-			output_file_lines = [] # start with empty list
-			for line in input_file: # iterate throgh each line in the input file
-				if "@prefix" in line: # if the line contains "@prefix"
-					row_count += 1 # add one to row count
-					output_file_lines.append(line)
+	punctuation = edited_line.split('"')[-1]
 
-				elif "rdfs:" in line:
-					if "rml.py SNAPSHOT:" in line:
-						spacing = line.split('rdfs:')[0]
-						edited_line = line.strip()
-						punctuation = "] " + edited_line[-1] # see if line ends in ;, ,, or .
-						edited_line = edited_line.strip(punctuation)
-						edited_line = edited_line.strip()
-						edited_line = spacing + edited_line + f"^^xsd:dateTime {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else:
-						output_file_lines.append(line)
+	edited_line = edited_line.strip(punctuation)
+	edited_line = spacing + edited_line + f"^^xsd:{date_type}" + punctuation + "\n"
 
-				else:
-					# isolate the name of the property
-					property_ = line.split('bf:')[-1]
-					property_ = property_.split(' ')[0]
+	return edited_line
 
-					if property_ in bf_date_prop_list:
-						spacing = line.split('bf:')[0] # preserve whitespace at the beginning of the line
-						edited_line = line.strip() # remove whitespaces
-						dash_count = 0 # start at 0
-						for character in edited_line: # iterate through characters in line
-							if character == "-":
-								dash_count += 1 # count the number of dashes
-						punctuation = edited_line[-1] # see if line ends in ;, ,, or .
-						if dash_count == 0: # no dashes, e.g. "1886"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYear {punctuation}\n"
-						elif dash_count == 1: # one dash, e.g. "1886-02"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYearMonth {punctuation}\n"
-						elif dash_count == 2: # two dashes, e.g. "1886-02-14"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:date {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else: # if the line does not have a date in it
-						output_file_lines.append(line) # add it to list with no edits
-
-			output_file_lines[row_count] = output_file_lines[row_count].strip() + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n" # add prefix for datatypes after last prefix
-		with open(f"output/{currentDate}/instance/{instance}", "w") as output_file:
-			for line in output_file_lines:
-				output_file.write(line)
-
-def fix_item_output(itemList):
-	for item in itemList:
-		with open(f"output/{currentDate}/item/{item}", "r") as input_file: # open it up
-			row_count = 0 # start at 0
-			output_file_lines = [] # start with empty list
-			for line in input_file: # iterate throgh each line in the input file
-				if "@prefix" in line: # if the line contains "@prefix"
-					row_count += 1 # add one to row count
-					output_file_lines.append(line)
-
-				elif "rdfs:" in line:
-					if "rml.py SNAPSHOT:" in line:
-						spacing = line.split('rdfs:')[0]
-						edited_line = line.strip()
-						punctuation = "] " + edited_line[-1] # see if line ends in ;, ,, or .
-						edited_line = edited_line.strip(punctuation)
-						edited_line = edited_line.strip()
-						edited_line = spacing + edited_line + f"^^xsd:dateTime {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else:
-						output_file_lines.append(line)
-
-				else:
-					# isolate the name of the property
-					property_ = line.split('bf:')[-1]
-					property_ = property_.split(' ')[0]
-
-					if property_ in bf_date_prop_list:
-						spacing = line.split('bf:')[0] # preserve whitespace at the beginning of the line
-						edited_line = line.strip() # remove whitespaces
-						dash_count = 0 # start at 0
-						for character in edited_line: # iterate through characters in line
-							if character == "-":
-								dash_count += 1 # count the number of dashes
-						punctuation = edited_line[-1] # see if line ends in ;, ,, or .
-						if dash_count == 0: # no dashes, e.g. "1886"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYear {punctuation}\n"
-						elif dash_count == 1: # one dash, e.g. "1886-02"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:gYearMonth {punctuation}\n"
-						elif dash_count == 2: # two dashes, e.g. "1886-02-14"
-							edited_line = edited_line.strip(punctuation)
-							edited_line = edited_line.strip()
-							edited_line = spacing + edited_line + f"^^xsd:date {punctuation}\n"
-						output_file_lines.append(edited_line) # add to list
-					else: # if the line does not have a date in it
-						output_file_lines.append(line) # add it to list with no edits
-
-			output_file_lines[row_count] = output_file_lines[row_count].strip() + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n" # add prefix for datatypes after last prefix
-		with open(f"output/{currentDate}/item/{item}", "w") as output_file:
-			for line in output_file_lines:
-				output_file.write(line)
-
-work1List = os.listdir(f"output/{currentDate}/work_1/")
-work2List = os.listdir(f"output/{currentDate}/work_2/")
-instanceList = os.listdir(f"output/{currentDate}/instance/")
-itemList = os.listdir(f"output/{currentDate}/item/")
+"""Lists and Dictionaries"""
 
 bf_date_prop_list = ["date", "originDate", "legalDate", "copyrightDate", "changeDate", "creationDate", "generationDate"]
 
-#
+work_1_list = os.listdir(f"../output/{currentDate}/work_1_ttl/")
+work_2_list = os.listdir(f"../output/{currentDate}/work_2_ttl/")
+instance_list = os.listdir(f"../output/{currentDate}/instance_ttl/")
+item_list = os.listdir(f"../output/{currentDate}/item_ttl/")
 
-fix_work_1_output(work1List)
-fix_work_2_output(work2List)
-fix_instance_output(instanceList)
-fix_item_output(itemList)
+resource_dict = {"work_1": work_1_list, "work_2": work_2_list, "instance": instance_list, "item": item_list}
+
+###
+
+for entity in resource_dict.keys():
+	resource_list = resource_dict[entity]
+	for resource in resource_list:
+		with open(f"../output/{currentDate}/{entity}_ttl/{resource}", "r") as input_file:
+			output_file_lines = []
+
+			row_count = 0
+
+			for line in input_file:
+				# preserve prefixes
+				if "@prefix" in line:
+					output_file_lines.append(line)
+					row_count += 1
+
+				elif "rml.py SNAPSHOT:" in line:
+					edited_line = type_snapshot(line)
+					output_file_lines.append(edited_line)
+
+				else:
+					prop = line.split('bf:')[-1]
+					prop = line.split(' ')[0]
+
+					if prop in bf_date_prop_list:
+						date_type = determine_date_type(line)
+						edited_line = type_date(line, date_type)
+						output_file_lines.append(edited_line)
+					else:
+						output_file_lines.append(line)
+			output_file_lines[row_count] = output_file_lines[row_count].strip() + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n" # add prefix for datatypes after last prefix
+		with open(f"../output/{currentDate}/{entity}_ttl/{resource}", "w") as output_file:
+			for line in output_file_lines:
+				output_file.write(line)
--- rda-to-bf-conversion/type_dates.py
+++ rda-to-bf-conversion/type_dates.py
@@ -1,94 +1,61 @@
 import os
 from datetime import date
+import xml.etree.ElementTree as ET
 
 """Variables"""
 
 today = date.today()
 currentDate = str(today).replace('-','_')
 
-"""Functions"""
-
-def type_snapshot(line):
-	spacing = line.split('rdfs:')[0]
-	edited_line = line.strip()
-
-	punctuation = edited_line.split('"')[-1]
-
-	edited_line = edited_line.strip(punctuation)
-	edited_line = spacing + edited_line + "^^xsd:dateTime" + punctuation + "\n"
-
-	return edited_line
-
-def determine_date_type(line):
-	value = line.split('"')[1]
-
-	dash_count = 0
-	for character in value:
-		if character == '-':
-			dash_count += 1
-
-	if dash_count == 0: # e.g. "1886"
-		date_type = "gYear"
-	elif dash_count == 1: # e.g. "1886-02"
-		date_type = "gYearMonth"
-	elif dash_count == 2: # e.g. "1886-02-14"
-		date_type = "date"
-
-	return date_type
-
-def type_date(line, date_type):
-	spacing = line.split('bf:')[0]
-	edited_line = line.strip()
-
-	punctuation = edited_line.split('"')[-1]
-
-	edited_line = edited_line.strip(punctuation)
-	edited_line = spacing + edited_line + f"^^xsd:{date_type}" + punctuation + "\n"
-
-	return edited_line
-
 """Lists and Dictionaries"""
 
 bf_date_prop_list = ["date", "originDate", "legalDate", "copyrightDate", "changeDate", "creationDate", "generationDate"]
 
-work_1_list = os.listdir(f"../output/{currentDate}/work_1_ttl/")
-work_2_list = os.listdir(f"../output/{currentDate}/work_2_ttl/")
-instance_list = os.listdir(f"../output/{currentDate}/instance_ttl/")
-item_list = os.listdir(f"../output/{currentDate}/item_ttl/")
+work_1_list = os.listdir(f"../output/{currentDate}/work_1_xml/")
+work_2_list = os.listdir(f"../output/{currentDate}/work_2_xml/")
+instance_list = os.listdir(f"../output/{currentDate}/instance_xml/")
+item_list = os.listdir(f"../output/{currentDate}/item_xml/")
 
 resource_dict = {"work_1": work_1_list, "work_2": work_2_list, "instance": instance_list, "item": item_list}
+
+"""Functions"""
+def determine_date_type(value):
+	num_of_dashes = 0
+	time_included = False
+	for character in value:
+		if character == '-':
+			num_of_dashes += 1
+		elif character == ':':
+			time_included = True
+
+
+	if num_of_dashes == 0: # e.g. "1886"
+		date_type = "gYear"
+	elif num_of_dashes == 1: # e.g. "1886-02"
+		date_type = 'gYearMonth'
+	elif num_of_dashes == 2: # e.g. "1886-02-14" or "2021-03-17T12:00:00+00:00"
+		if time_included == True:
+			date_type = 'dateTime'
+		else:
+			date_type = 'date'
+
+	return date_type
+
+def add_dates_in_xml(currentDate, entity, file):
+	# open xml parser
+	tree = ET.parse(f'../output/{currentDate}/{entity}_xml/{file}')
+	root = tree.getroot()
+
+	for child in root:
+		for prop in child:
+			if prop.tag.split('}')[-1] in bf_date_prop_list:
+				date_type = determine_date_type(prop.text)
+				prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}datatype', f"http://www.w3.org/2001/XMLSchema#{date_type}")
+
+	tree.write(f'../output/{currentDate}/{entity}_xml/{file}')
 
 ###
 
 for entity in resource_dict.keys():
-	resource_list = resource_dict[entity]
-	for resource in resource_list:
-		with open(f"../output/{currentDate}/{entity}_ttl/{resource}", "r") as input_file:
-			output_file_lines = []
-
-			row_count = 0
-
-			for line in input_file:
-				# preserve prefixes
-				if "@prefix" in line:
-					output_file_lines.append(line)
-					row_count += 1
-
-				elif "rml.py SNAPSHOT:" in line:
-					edited_line = type_snapshot(line)
-					output_file_lines.append(edited_line)
-
-				else:
-					prop = line.split('bf:')[-1]
-					prop = line.split(' ')[0]
-
-					if prop in bf_date_prop_list:
-						date_type = determine_date_type(line)
-						edited_line = type_date(line, date_type)
-						output_file_lines.append(edited_line)
-					else:
-						output_file_lines.append(line)
-			output_file_lines[row_count] = output_file_lines[row_count].strip() + "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n" # add prefix for datatypes after last prefix
-		with open(f"../output/{currentDate}/{entity}_ttl/{resource}", "w") as output_file:
-			for line in output_file_lines:
-				output_file.write(line)
+	for resource in resource_dict[entity]:
+		add_dates_in_xml(currentDate, entity, resource)
