5efdd7f8a21dd2e5631f314b3bb0eb7c5975e6a6
mcm104 <mcm104@uw.edu>
2021-03-11 16:51:15 -0800

    Add option for reserialization

--- rda-to-bf-conversion/type_dates.py
+++ rda-to-bf-conversion/type_dates.py
@@ -1,6 +1,25 @@
-import os
 from datetime import date
+import os
+from progress.bar import Bar
+from rdflib import *
 import xml.etree.ElementTree as ET
+
+"""Namespaces"""
+LDP = Namespace('http://www.w3.org/ns/ldp#')
+bf = Namespace('http://id.loc.gov/ontologies/bibframe/')
+bflc = Namespace('http://id.loc.gov/ontologies/bflc/')
+dbo = Namespace('http://dbpedia.org/ontology/')
+madsrdf = Namespace('http://www.loc.gov/mads/rdf/v1#')
+owl = Namespace('http://www.w3.org/2002/07/owl#')
+rdac = Namespace('http://rdaregistry.info/Elements/c/')
+rdae = Namespace('http://rdaregistry.info/Elements/e/')
+rdai = Namespace('http://rdaregistry.info/Elements/i/')
+rdam = Namespace('http://rdaregistry.info/Elements/m/')
+rdamdt = Namespace('http://rdaregistry.info/Elements/m/datatype/')
+rdau = Namespace('http://rdaregistry.info/Elements/u/')
+rdaw = Namespace('http://rdaregistry.info/Elements/w/')
+rdax = Namespace('https://doi.org/10.6069/uwlib.55.d.4#')
+sin = Namespace('http://sinopia.io/vocabulary/')
 
 """Variables"""
 
@@ -19,6 +38,26 @@ item_list = os.listdir(f"../output/{currentDate}/item_xml/")
 resource_dict = {"work_1": work_1_list, "work_2": work_2_list, "instance": instance_list, "item": item_list}
 
 """Functions"""
+def reserialize(file):
+	"""Reserialize with rdflib to fix namespaces and UTf-8"""
+	g = Graph()
+	g.bind('bf', bf)
+	g.bind('bflc', bflc)
+	g.bind('dbo', dbo)
+	g.bind('madsrdf', madsrdf)
+	g.bind('owl', owl)
+	g.bind('rdac', rdac)
+	g.bind('rdae', rdae)
+	g.bind('rdai', rdai)
+	g.bind('rdam', rdam)
+	g.bind('rdamdt', rdamdt)
+	g.bind('rdau', rdau)
+	g.bind('rdaw', rdaw)
+	g.bind('rdax', rdax)
+	g.bind('sin', sin)
+	g.load(f'file:{file}', format='xml')
+	g.serialize(destination=f'{file}', format='xml')
+
 def determine_date_type(value):
 	num_of_dashes = 0
 	time_included = False
@@ -27,7 +66,6 @@ def determine_date_type(value):
 			num_of_dashes += 1
 		elif character == ':':
 			time_included = True
-
 
 	if num_of_dashes == 0: # e.g. "1886"
 		date_type = "gYear"
@@ -51,11 +89,23 @@ def add_dates_in_xml(currentDate, entity, file):
 			if prop.tag.split('}')[-1] in bf_date_prop_list:
 				date_type = determine_date_type(prop.text)
 				prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}datatype', f"http://www.w3.org/2001/XMLSchema#{date_type}")
+			elif prop.tag.split('}')[-1] == 'label':
+				if prop.text == None:
+					pass
+				elif prop.text[0:15] == "rml.py SNAPSHOT":
+					date_type = determine_date_type(prop.text)
+					prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}datatype', f"http://www.w3.org/2001/XMLSchema#{date_type}")
 
 	tree.write(f'../output/{currentDate}/{entity}_xml/{file}')
 
-###
+	reserialize(f'../output/{currentDate}/{entity}_xml/{file}')
 
+###
+num_of_resources = len(work_1_list) + len(work_2_list) + len(instance_list) + len(item_list)
+
+bar = Bar(max=num_of_resources, suffix='%(percent)d%%') # progress bar
 for entity in resource_dict.keys():
 	for resource in resource_dict[entity]:
 		add_dates_in_xml(currentDate, entity, resource)
+		bar.next()
+bar.finish()
