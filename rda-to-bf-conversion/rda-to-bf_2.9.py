8ae1c2bff1fe88603703e3cc2fdd5192f23a00a9
mcm104 <mcm104@uw.edu>
2021-03-19 14:24:38 -0700

    Remove Sinopia-specific scripts

---
+++ rda-to-bf-conversion/rda-to-bf_2.9.py
@@ -0,0 +1,26 @@
+"""Retrieve RDA Data"""
+import save_rda_locally
+
+print("...\nCleaning input data")
+
+print(">> Removing extra descriptions")
+import remove_extra_descriptions
+
+print(">> Fixing IRIs typed as literals")
+import fix_URIs
+
+print(">> Removing blank copyright dates")
+import fix_copyright_dates
+
+"""Transform RDA to BIBFRAME in XML"""
+import transform_rda_to_bf
+
+print("...\nCleaning output data")
+
+print(">> Replacing RDA IRIs with BIBFRAME IRIs")
+import fix_related_IRIs
+
+print(">> Adding datatypes to dates")
+import type_dates
+
+print("...\nDone!")
