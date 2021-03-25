5122dfa53f81158cb0bf31319b9e012ddef3e0ac
mcm104 <mcm104@uw.edu>
2021-03-25 13:50:47 -0700

    Tested 2021_03_25

--- rda-to-bf-conversion-for-sinopia/fix_related_IRIs.py
+++ rda-to-bf-conversion-for-sinopia/fix_related_IRIs.py
@@ -3,7 +3,7 @@ from datetime import date
 import os
 from progress.bar import Bar
 from rdflib import *
-import time
+from timeit import default_timer as timer
 import xml.etree.ElementTree as ET
 
 """Namespaces"""
@@ -70,17 +70,21 @@ def fix_related_IRIs(RDA_ID, BF_ID, entity, file):
 			if '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource' in prop.attrib.keys(): # if the value is an IRI...
 				RDA_IRI = f'https://api.sinopia.io/resource/{RDA_ID}'
 				if prop.attrib['{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource'] == RDA_IRI: # if that IRI is the RDA IRI in question
-					BF_IRI = f'https://api.sinopia.io/resource/{BF_ID}'
-					prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource', BF_IRI) # replace it with its equivalent BF IRI
-					edit = True
+					if prop.tag != '{http://www.w3.org/2002/07/owl#}sameAs': # and it is NOT our owl:sameAs triple, which should be left alone
+						BF_IRI = f'https://api.sinopia.io/resource/{BF_ID}'
+						prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource', BF_IRI) # replace it with its equivalent BF IRI
+						edit = True
 
 	if edit == True: # only rewrite / reserialize if an edit was made
 		tree.write(f'../output/{currentDate}/{entity}_xml/{file}')
 
 		reserialize(f'../output/{currentDate}/{entity}_xml/{file}')
 
+	return edit
+
 ###
 
+start = timer()
 with open(f"RDA_BF_IRI_list_{currentDate}.csv", mode="r") as key_file:
 	"""Get number of IRI pairs to iterate through"""
 	csv_reader = csv.reader(key_file, delimiter=',')
@@ -93,8 +97,9 @@ with open(f"RDA_BF_IRI_list_{currentDate}.csv", mode="r") as key_file:
 	"""For each resource, look for IRI pairs and make appropriate changes"""
 	csv_reader = csv.reader(key_file, delimiter=',')
 	line_count = 0
+	num_of_edits = 0
 
-	bar = Bar(max=total_line_count, suffix='%(percent)d%%') # progress bar
+	bar = Bar(">> Replacing RDA IRIs with BIBFRAME IRIs", max=total_line_count, suffix='%(percent)d%%') # progress bar
 	for line in csv_reader:
 		if line_count == 0: # skip header row
 			pass
@@ -104,7 +109,13 @@ with open(f"RDA_BF_IRI_list_{currentDate}.csv", mode="r") as key_file:
 
 			for entity in resource_dict.keys():
 				for resource in resource_dict[entity]:
-					fix_related_IRIs(RDA_ID, BF_ID, entity, resource)
+					edit_made = fix_related_IRIs(RDA_ID, BF_ID, entity, resource)
+					if edit_made == True:
+						num_of_edits += 1
 		line_count += 1
 		bar.next()
+	end = timer()
 	bar.finish()
+
+print(f"Edits made: {num_of_edits}")
+print(f"Elapsed time: {round((end - start), 1)} s")
