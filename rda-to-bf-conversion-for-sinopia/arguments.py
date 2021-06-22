178148f2d885dee64f10044dca89a01cd2b35892
mcm104 <mcm104@uw.edu>
2021-06-22 13:28:01 -0700

    Edit 2021-05-12

---
+++ rda-to-bf-conversion-for-sinopia/arguments.py
@@ -0,0 +1,12 @@
+import argparse
+
+def define_arg():
+	parser = argparse.ArgumentParser(description='Convert data from RDA/RDF to BIBFRAME and post to Sinopia.')
+	parser.add_argument('-s', '--source', default='https://api.sinopia.io/resource?limit=1000&group=washington', help='URI for RDA data')
+	parser.add_argument('-i', '--input', default='RDA', help='desired location of RDA files')
+	parser.add_argument('-o', '--output', default='BIBFRAME', help='desired location of BIBFRAME files')
+	parser.add_argument('-v', '--version', action='version', version='1.26')
+
+	args = parser.parse_args()
+
+	return args
