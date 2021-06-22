45423f9cee7d135f9d0ce0c2048ee3648efd2bdc
mcm104 <mcm104@uw.edu>
2021-06-22 13:28:21 -0700

    Edit 2021-05-25

---
+++ rda-to-bf-conversion-for-sinopia/rda-to-bf-for-sinopia.py
@@ -0,0 +1,40 @@
+import os
+
+"""Retrieve RDA Data"""
+print("Saving RDA data locally in RDF/XML")
+import save_rda_locally
+
+print("...\nCleaning input data")
+
+"""Remove descriptions that are not about the resource in question"""
+import remove_extra_descriptions
+
+"""Fix IRIs that have been typed as literals"""
+import fix_URIs
+
+"""Remove empty copyright dates"""
+import fix_copyright_dates
+
+#os.system('cp -r RDA/2021_05_11 RDA/do_not_touch')
+
+"""Transform RDA to BIBFRAME in XML"""
+print("...\nTransforming RDA data into BIBFRAME in RDF/XML")
+import transform_rda_to_bf
+
+print("...\nCleaning output data")
+
+"""Replacing RDA IRIs with BIBFRAME IRIs"""
+import fix_related_IRIs
+
+"""Adding datatypes to dates"""
+import type_dates
+
+#os.system('cp -r BIBFRAME/2021_05_12 RDA/do_not_touch')
+
+"""Prepare JSON-LD for upload into Sinopia"""
+import edit_json
+
+"""Upload into Sinopia"""
+import post_to_sinopia
+
+print("...\nDone!")
