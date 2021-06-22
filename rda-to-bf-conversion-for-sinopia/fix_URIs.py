d756d89966c5b63c83c7df271e266427cfe897f1
mcm104 <mcm104@uw.edu>
2021-06-22 12:58:22 -0700

    Edit 2021-05-06

--- rda-to-bf-conversion-for-sinopia/fix_URIs.py
+++ rda-to-bf-conversion-for-sinopia/fix_URIs.py
@@ -1,47 +1,21 @@
+from arguments import define_arg
 import os
 from datetime import date
 from progress.bar import Bar
 from rdflib import *
+from reserialize import reserialize
 from timeit import default_timer as timer
 import xml.etree.ElementTree as ET
 
-"""Namespaces"""
-bf = Namespace('http://id.loc.gov/ontologies/bibframe/')
-bflc = Namespace('http://id.loc.gov/ontologies/bflc/')
-madsrdf = Namespace('http://www.loc.gov/mads/rdf/v1#')
-rdac = Namespace('http://rdaregistry.info/Elements/c/')
-rdae = Namespace('http://rdaregistry.info/Elements/e/')
-rdai = Namespace('http://rdaregistry.info/Elements/i/')
-rdam = Namespace('http://rdaregistry.info/Elements/m/')
-rdamdt = Namespace('http://rdaregistry.info/Elements/m/datatype/')
-rdaw = Namespace('http://rdaregistry.info/Elements/w/')
-rdax = Namespace('https://doi.org/10.6069/uwlib.55.d.4#')
-sin = Namespace('http://sinopia.io/vocabulary/')
-
 """Functions"""
 
-def reserialize(file):
-	"""Reserialize with rdflib to fix namespaces and UTf-8"""
-	g = Graph()
-	g.bind('bf', bf)
-	g.bind('bflc', bflc)
-	g.bind('madsrdf', madsrdf)
-	g.bind('rdac', rdac)
-	g.bind('rdae', rdae)
-	g.bind('rdai', rdai)
-	g.bind('rdam', rdam)
-	g.bind('rdamdt', rdamdt)
-	g.bind('rdaw', rdaw)
-	g.bind('rdax', rdax)
-	g.bind('sin', sin)
-	g.load(f'file:{file}', format='xml')
-	g.serialize(destination=f'{file}', format='xml')
+def fix_URIs(entity, file, input_location):
+	"""Find URIs that have been entered as literals, and correct them so they are typed as URIs"""
 
-def fix_URIs(entity, file):
-	edit_made = False
+	num_of_edits = 0
 
 	# open xml parser
-	tree = ET.parse(f'../input/{currentDate}/{entity}/{file}')
+	tree = ET.parse(f'{input_location}/{currentDate}/{entity}/{file}')
 	root = tree.getroot()
 
 	for child in root: # for each node...
@@ -51,21 +25,21 @@ def fix_URIs(entity, file):
 					IRI = prop.text
 					prop.clear() # clear literal value
 					prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource', IRI) # add IRI as attribute
-					edit_made = True
+					num_of_edits += 1
 
-					tree.write(f'../input/{currentDate}/{entity}/{file}')
+					tree.write(f'{input_location}/{currentDate}/{entity}/{file}')
 
-					reserialize(f'../input/{currentDate}/{entity}/{file}')
+					reserialize(f'{input_location}/{currentDate}/{entity}/{file}', f'{input_location}/{currentDate}/{entity}/{file}', 'xml')
 			elif '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource' in prop.attrib.keys():
 				if prop.attrib['{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource'] == 'file:///home/forge/rda.metadataregistry.org/storage/repos/projects/177/xml/termList/rdacc1003':
 					prop.clear()
 					prop.set('{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource', 'http://rdaregistry.info/termList/RDAColourContent/1003')
-					edit_made = True
+					num_of_edits += 1
 
-					tree.write(f'../input/{currentDate}/{entity}/{file}')
+					tree.write(f'{input_location}/{currentDate}/{entity}/{file}')
 
-					reserialize(f'../input/{currentDate}/{entity}/{file}')
-	return edit_made
+					reserialize(f'{input_location}/{currentDate}/{entity}/{file}', f'{input_location}/{currentDate}/{entity}/{file}', 'xml')
+	return num_of_edits
 
 """Variables"""
 
@@ -73,14 +47,18 @@ def fix_URIs(entity, file):
 today = date.today()
 currentDate = str(today).replace('-', '_')
 
+# arguments from command line
+args = define_arg()
+input_location = args.input
+
 num_of_edits = 0
 
 """Lists and Dictionaries"""
 
-workList = os.listdir(f'../input/{currentDate}/work')
-expressionList = os.listdir(f'../input/{currentDate}/expression')
-manifestationList = os.listdir(f'../input/{currentDate}/manifestation')
-itemList = os.listdir(f'../input/{currentDate}/item')
+workList = os.listdir(f'{input_location}/{currentDate}/work')
+expressionList = os.listdir(f'{input_location}/{currentDate}/expression')
+manifestationList = os.listdir(f'{input_location}/{currentDate}/manifestation')
+itemList = os.listdir(f'{input_location}/{currentDate}/item')
 
 resource_dict = {"work": workList, "expression": expressionList, "manifestation": manifestationList, "item": itemList}
 
@@ -93,11 +71,10 @@ bar = Bar(">> Fixing IRIs typed as literals", max=num_of_resources, suffix='%(pe
 start = timer()
 for entity in resource_dict.keys():
 	for resource in resource_dict[entity]:
-		edit_made = fix_URIs(entity, resource)
-		if edit_made == True:
-			num_of_edits += 1
+		edits_made = fix_URIs(entity, resource, input_location)
+		num_of_edits += edits_made
 		bar.next()
 end = timer()
 bar.finish()
-print(f"Resources edited: {num_of_edits}")
+print(f"IRIs corrected: {num_of_edits}")
 print(f"Elapsed time: {round((end - start), 1)} s")
