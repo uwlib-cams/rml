a87e8b43561cd97ac0ccf84167d785964ac7fce7
mcm104 <mcm104@uw.edu>
2022-02-23 16:35:48 -0800

    Edit 2022-02-18

--- generateRML/functions/logical_source_functions.py
+++ generateRML/functions/logical_source_functions.py
@@ -2,43 +2,28 @@
 from rdflib import *
 
 """Lists"""
-from functions.lists import classificationLcc_props
-from functions.lists import classificationNlm_props
-from functions.lists import dissertationList
+#from functions.lists import classificationLcc_props
+#from functions.lists import classificationNlm_props
+#from functions.lists import dissertationList
 from functions.lists import expression_title_props
 from functions.lists import item_title_props
 from functions.lists import manifestation_title_props
-from functions.lists import provisionActivityDistributionList
-from functions.lists import provisionActivityManufactureList
-from functions.lists import provisionActivityProductionList
-from functions.lists import provisionActivityPublicationList
+#from functions.lists import provisionActivityDistributionList
+#from functions.lists import provisionActivityManufactureList
+#from functions.lists import provisionActivityProductionList
+#from functions.lists import provisionActivityPublicationList
 from functions.lists import work_title_props
 
 """Namespaces"""
-bf = Namespace('http://id.loc.gov/ontologies/bibframe/')
-bflc = Namespace('http://id.loc.gov/ontologies/bflc/')
-dbo = Namespace('http://dbpedia.org/ontology/')
 ex = Namespace('http://example.org/entity/')
-madsrdf = Namespace('http://www.loc.gov/mads/rdf/v1#')
 ql = Namespace('http://semweb.mmlab.be/ns/ql#')
-rdac = Namespace('http://rdaregistry.info/Elements/c/')
-rdae = Namespace('http://rdaregistry.info/Elements/e/')
-rdai = Namespace('http://rdaregistry.info/Elements/i/')
-rdam = Namespace('http://rdaregistry.info/Elements/m/')
-rdamdt = Namespace('http://rdaregistry.info/Elements/m/datatype/')
-rdau = Namespace('http://rdaregistry.info/Elements/u/')
-rdaw = Namespace('http://rdaregistry.info/Elements/w/')
-rdax = Namespace('https://doi.org/10.6069/uwlib.55.d.4#')
 rdf = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-rdfs = Namespace('http://www.w3.org/2000/01/rdf-schema#')
 rml = Namespace('http://semweb.mmlab.be/ns/rml#')
 rr = Namespace('http://www.w3.org/ns/r2rml#')
-schema = Namespace('http://schema.org/')
-sin = Namespace('http://sinopia.io/vocabulary/')
-skos = Namespace('http://www.w3.org/2004/02/skos/core#')
 
 """Functions"""
 def generate_main_logical_source(RML_graph, entity):
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_map = ex.WorkMap
 		default_path = Literal("!!work_filepath!!.xml")
@@ -56,8 +41,10 @@ def generate_main_logical_source(RML_graph, entity):
 		default_path = Literal("!!item_filepath!!.xml")
 		default_iterator = Literal("/rdf:RDF/rdf:Description[rdf:type/@rdf:resource='http://rdaregistry.info/Elements/c/C10003']")
 
+	"""Set other variables"""
 	main_logical_source = BNode()
 
+	"""Add triples"""
 	RML_graph.add((default_map, rdf.type, rr.TriplesMap))
 	RML_graph.add((default_map, rml.logicalSource, main_logical_source))
 	RML_graph.add((main_logical_source, rml.source, default_path))
@@ -67,6 +54,9 @@ def generate_main_logical_source(RML_graph, entity):
 	return RML_graph
 
 def generate_IRI_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical sources for blank nodes where the property takes an IRI"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 	elif entity.lower() == "expression":
@@ -76,10 +66,12 @@ def generate_IRI_logical_source(RML_graph, entity, map_name, property_number):
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
 	IRI_logical_source = BNode()
 	IRI_iterator = Literal(f"/rdf:RDF/rdf:Description/{property_number}/@rdf:resource")
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, IRI_logical_source))
 	RML_graph.add((IRI_logical_source, rml.source, default_path))
@@ -89,6 +81,11 @@ def generate_IRI_logical_source(RML_graph, entity, map_name, property_number):
 	return RML_graph
 
 def generate_neutral_literal_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical source for blank nodes where the property takes a literal"""
+	"""This applies to literals regardless of whether or not they have a language tag (i.e. neutral)"""
+	"""The language tags for these literals (if they exist) are *NOT* recorded"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 	elif entity.lower() == "expression":
@@ -98,10 +95,12 @@ def generate_neutral_literal_logical_source(RML_graph, entity, map_name, propert
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
 	neutral_literal_logical_source = BNode()
 	neutral_literal_iterator = Literal(f"/rdf:RDF/rdf:Description/{property_number}[not(@rdf:resource)]")
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, neutral_literal_logical_source))
 	RML_graph.add((neutral_literal_logical_source, rml.source, default_path))
@@ -111,6 +110,11 @@ def generate_neutral_literal_logical_source(RML_graph, entity, map_name, propert
 	return RML_graph
 
 def generate_lang_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical source for blank nodes where the property takes a literal"""
+	"""This will only apply to literals that have a language tag (i.e. lang)"""
+	"""The language tags for these literals *ARE* recorded"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 	elif entity.lower() == "expression":
@@ -120,10 +124,12 @@ def generate_lang_logical_source(RML_graph, entity, map_name, property_number):
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
 	lang_logical_source = BNode()
 	lang_iterator = Literal(f"/rdf:RDF/rdf:Description/{property_number}[not(@rdf:resource)][@lang]")
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, lang_logical_source))
 	RML_graph.add((lang_logical_source, rml.source, default_path))
@@ -132,7 +138,12 @@ def generate_lang_logical_source(RML_graph, entity, map_name, property_number):
 
 	return RML_graph
 
-def generate_constant_logical_source(RML_graph, entity, map_name, property_number):
+def generate_not_lang_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical source for blank nodes where the property takes a literal"""
+	"""This will only apply to literals that do *NOT* have a language tag (i.e. not_lang)"""
+	"""The language tags for these literals are *NOT* recorded (because they don't exist)"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 	elif entity.lower() == "expression":
@@ -142,8 +153,34 @@ def generate_constant_logical_source(RML_graph, entity, map_name, property_numbe
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
-	constant_logical_source = BNode()
+	not_lang_logical_source = BNode()
+	not_lang_iterator = Literal(f"/rdf:RDF/rdf:Description/{property_number}[not(@rdf:resource) and not(@lang)]")
+
+	"""Add triples"""
+	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
+	RML_graph.add((map_name, rml.logicalSource, not_lang_logical_source))
+	RML_graph.add((not_lang_logical_source, rml.source, default_path))
+	RML_graph.add((not_lang_logical_source, rml.referenceFormulation, ql.XPath))
+	RML_graph.add((not_lang_logical_source, rml.iterator, not_lang_iterator))
+
+	return RML_graph
+
+def generate_constant_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical source for blank nodes that only contain a constant IRI or literal"""
+
+	"""Set defaults based on entity"""
+	if entity.lower() == "work":
+		default_path = Literal("!!work_filepath!!.xml")
+	elif entity.lower() == "expression":
+		default_path = Literal("!!expression_filepath!!.xml")
+	elif entity.lower() == "manifestation":
+		default_path = Literal("!!manifestation_filepath!!.xml")
+	elif entity.lower() == "item":
+		default_path = Literal("!!item_filepath!!.xml")
+
+	"""Set defaults based on value type"""
 	if "Literal" in map_name:
 		if "Not_" in map_name and "Lang_" in map_name:
 			constant_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_number}[not(@rdf:resource) and not(@lang)]]")
@@ -154,7 +191,11 @@ def generate_constant_logical_source(RML_graph, entity, map_name, property_numbe
 	elif "IRI" in map_name:
 		constant_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_number}[@rdf:resource]]")
 
+	"""Set other variables"""
+	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
+	constant_logical_source = BNode()
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, constant_logical_source))
 	RML_graph.add((constant_logical_source, rml.source, default_path))
@@ -163,29 +204,10 @@ def generate_constant_logical_source(RML_graph, entity, map_name, property_numbe
 
 	return RML_graph
 
-def generate_not_lang_logical_source(RML_graph, entity, map_name, property_number):
-	if entity.lower() == "work":
-		default_path = Literal("!!work_filepath!!.xml")
-	elif entity.lower() == "expression":
-		default_path = Literal("!!expression_filepath!!.xml")
-	elif entity.lower() == "manifestation":
-		default_path = Literal("!!manifestation_filepath!!.xml")
-	elif entity.lower() == "item":
-		default_path = Literal("!!item_filepath!!.xml")
-
-	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
-	not_lang_logical_source = BNode()
-	not_lang_iterator = Literal(f"/rdf:RDF/rdf:Description/{property_number}[not(@rdf:resource) and not(@lang)]")
-
-	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
-	RML_graph.add((map_name, rml.logicalSource, not_lang_logical_source))
-	RML_graph.add((not_lang_logical_source, rml.source, default_path))
-	RML_graph.add((not_lang_logical_source, rml.referenceFormulation, ql.XPath))
-	RML_graph.add((not_lang_logical_source, rml.iterator, not_lang_iterator))
-
-	return RML_graph
-
 def generate_provact_logical_source(RML_graph, class_name, map_name):
+	"""Logical source for blank nodes that are the value of bf:provisionActivity"""
+
+	"""Set defaults based on blank node class"""
 	if class_name == "Distribution":
 		property_numbers = " or ".join(provisionActivityDistributionList)
 	elif class_name == "Manufacture":
@@ -195,11 +217,13 @@ def generate_provact_logical_source(RML_graph, class_name, map_name):
 	elif class_name == "Publication":
 		property_numbers = " or ".join(provisionActivityPublicationList)
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
 	provact_logical_source = BNode()
 	provact_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_numbers}]")
 	manifestation_path = Literal("!!manifestation_filepath!!.xml")
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, provact_logical_source))
 	RML_graph.add((provact_logical_source, rml.source, manifestation_path))
@@ -209,6 +233,9 @@ def generate_provact_logical_source(RML_graph, class_name, map_name):
 	return RML_graph
 
 def generate_title_logical_source(RML_graph, entity, map_name):
+	"""Logical source for blank nodes that are the value of bf:title"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 		property_numbers = " or ".join(work_title_props)
@@ -222,10 +249,12 @@ def generate_title_logical_source(RML_graph, entity, map_name):
 		default_path = Literal("!!item_filepath!!.xml")
 		property_numbers = " or ".join(item_title_props)
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
 	title_logical_source = BNode()
 	title_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_numbers}]")
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, title_logical_source))
 	RML_graph.add((title_logical_source, rml.source, default_path))
@@ -235,20 +264,26 @@ def generate_title_logical_source(RML_graph, entity, map_name):
 	return RML_graph
 
 def generate_classification_logical_source(RML_graph, entity, map_name):
+	"""Logical source for blank nodes classed as bf:ClassificationLcc or bf:ClassificationNlm"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "manifestation":
 		default_path = Literal("!!manifestation_filepath!!.xml")
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
+	"""Set defaults based on blank node class"""
 	if "Lcc" in map_name:
 		property_numbers = " or ".join(classificationLcc_props)
 	elif "Nlm" in map_name:
 		property_numbers = " or ".join(classificationNlm_props)
 
+	"""Set other variables"""
 	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
 	classification_logical_source = BNode()
 	classification_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_numbers}]")
 
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, classification_logical_source))
 	RML_graph.add((classification_logical_source, rml.source, default_path))
@@ -257,7 +292,31 @@ def generate_classification_logical_source(RML_graph, entity, map_name):
 
 	return RML_graph
 
+def generate_dissertation_logical_source(RML_graph, map_name):
+	"""Logical source for blank nodes classed as bf:Dissertation"""
+
+	"""Set variables"""
+	work_path = Literal("!!work_filepath!!.xml")
+	property_numbers = " or ".join(dissertationList)
+	dissertation_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_numbers}]")
+	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
+	dissertation_logical_source = BNode()
+
+	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
+	RML_graph.add((map_name, rml.logicalSource, dissertation_logical_source))
+	RML_graph.add((dissertation_logical_source, rml.source, work_path))
+	RML_graph.add((dissertation_logical_source, rml.referenceFormulation, ql.XPath))
+	RML_graph.add((dissertation_logical_source, rml.iterator, dissertation_iterator))
+
+	return RML_graph
+
 def generate_lang_nosplit_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical source for "no-split" blank nodes"""
+	# See: https://github.com/uwlib-cams/rml/tree/master/generateRML/functions#nosplit_bnode_list
+	"""If generated for a literal, this will only apply to literals that have a language tag (i.e. lang)"""
+	"""The language tags for these literals *ARE* recorded"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 	elif entity.lower() == "expression":
@@ -267,13 +326,17 @@ def generate_lang_nosplit_logical_source(RML_graph, entity, map_name, property_n
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
-	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
-	lang_nosplit_logical_source = BNode()
+	"""Set defaults based on value type"""
 	if "IRI" in map_name:
 		lang_nosplit_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_number}[@rdf:resource]]")
 	else:
 		lang_nosplit_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_number}[not(@rdf:resource)][@lang]]")
 
+	"""Set other variables"""
+	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
+	lang_nosplit_logical_source = BNode()
+
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, lang_nosplit_logical_source))
 	RML_graph.add((lang_nosplit_logical_source, rml.source, default_path))
@@ -283,6 +346,12 @@ def generate_lang_nosplit_logical_source(RML_graph, entity, map_name, property_n
 	return RML_graph
 
 def generate_not_lang_nosplit_logical_source(RML_graph, entity, map_name, property_number):
+	"""Logical source for "no-split" blank nodes"""
+	# See: https://github.com/uwlib-cams/rml/tree/master/generateRML/functions#nosplit_bnode_list
+	"""If generated for a literal, this will only apply to literals that do *NOT* have a language tag (i.e. not_lang)"""
+	"""The language tags for these literals are *NOT* recorded (because they don't exist)"""
+
+	"""Set defaults based on entity"""
 	if entity.lower() == "work":
 		default_path = Literal("!!work_filepath!!.xml")
 	elif entity.lower() == "expression":
@@ -292,32 +361,21 @@ def generate_not_lang_nosplit_logical_source(RML_graph, entity, map_name, proper
 	elif entity.lower() == "item":
 		default_path = Literal("!!item_filepath!!.xml")
 
-	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
-	not_lang_nosplit_logical_source = BNode()
+	"""Set defaults based on value type"""
 	if "IRI" in map_name:
 		not_lang_nosplit_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_numbers}[@rdf:resource]]")
 	else:
 		not_lang_nosplit_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_number}[not(@rdf:resource) and not(@lang)]]")
 
+	"""Set other variables"""
+	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
+	not_lang_nosplit_logical_source = BNode()
+
+	"""Add triples"""
 	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
 	RML_graph.add((map_name, rml.logicalSource, not_lang_nosplit_logical_source))
 	RML_graph.add((not_lang_nosplit_logical_source, rml.source, default_path))
 	RML_graph.add((not_lang_nosplit_logical_source, rml.referenceFormulation, ql.XPath))
 	RML_graph.add((not_lang_nosplit_logical_source, rml.iterator, not_lang_nosplit_iterator))
-
-	return RML_graph
-
-def generate_dissertation_logical_source(RML_graph, map_name):
-	map_name = URIRef(f"http://example.org/entity/{map_name}Map")
-	dissertation_logical_source = BNode()
-	property_numbers = " or ".join(dissertationList)
-	dissertation_iterator = Literal(f"/rdf:RDF/rdf:Description[{property_numbers}]")
-	work_path = Literal("!!work_filepath!!.xml")
-
-	RML_graph.add((map_name, rdf.type, rr.TriplesMap))
-	RML_graph.add((map_name, rml.logicalSource, dissertation_logical_source))
-	RML_graph.add((dissertation_logical_source, rml.source, work_path))
-	RML_graph.add((dissertation_logical_source, rml.referenceFormulation, ql.XPath))
-	RML_graph.add((dissertation_logical_source, rml.iterator, dissertation_iterator))
 
 	return RML_graph
